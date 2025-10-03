da161edd3f22a8276b1a125f3fbe5cdb44619254
SivaTeja36 <vantasivateja@gmail.com>
2025-10-03 23:57:58 +0530

    Use shimmer ui

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
 import restaurants from '../utils/mockData'
 import RestaurantCard from './RestaurantCard'
+import Shimmer from './ShimmerCard'
 
 const Body = () => {
-    const [listOfRestaurants, updateListOfRestaurants] = useState(restaurants)
-    console.log(listOfRestaurants)
+    const [listOfRestaurants, updateListOfRestaurants] = useState([])
+
+    setTimeout(() => {
+        updateListOfRestaurants(restaurants)
+    }, 2000)
+
+    if (listOfRestaurants.length === 0) {
+        return <Shimmer />
+    } 
+
     return (
         <div className="body">
             <div className="filter"><button className='filter-btn' onClick={ () => {
