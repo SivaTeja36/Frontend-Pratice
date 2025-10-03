c41af47f481e167928f254675dc1e4da81605d6d
SivaTeja36 <vantasivateja@gmail.com>
2025-10-04 00:09:24 +0530

    Dynamic button

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -8,13 +8,9 @@ const Body = () => {
 
     setTimeout(() => {
         updateListOfRestaurants(restaurants)
-    }, 2000)
+    }, 1000)
 
-    if (listOfRestaurants.length === 0) {
-        return <Shimmer />
-    } 
-
-    return (
+    return listOfRestaurants.length === 0 ? <Shimmer /> : (
         <div className="body">
             <div className="filter"><button className='filter-btn' onClick={ () => {
                 const filteredRestaurantsList = listOfRestaurants.filter((restaurant) => restaurant.info.avgRating > 4.2);
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,30 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
-const Header = () => (
-  <div className="header">
-    <div className="logo-container">
-      <img
-        className="logo"
-        src={LOGO_URL}
-        alt="Logo"
-      />
+const Header = () => {
+  const [btnName, updateButtonName] = useState("Login out");
+  return(
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src={LOGO_URL}
+          alt="Logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button className="login" onClick={() => {
+            btnName === "Login out" ? updateButtonName("Login") : updateButtonName("Login out")
+          }}>{btnName}</button>
+        </ul>
+      </div>
     </div>
-    <div className="nav-items">
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact Us</li>
-        <li>Cart</li>
-      </ul>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Header;
